168939a5f859a9d504e8035f29fd8116a1aebe2e
Tran Tuan Khuong <tuankhuongk@gmail.com>
2024-11-27 15:59:16 +0700

    Add quanlybanhang_testCaseFixed

--- src/config/db.js
+++ src/config/db.js
@@ -4,17 +4,17 @@ const mysql = require("mysql2")
 const db = mysql.createConnection({
   host: "localhost", // Địa chỉ server của database
   user: "root", // Tên người dùng
-  password: "", // Mật khẩu
+  password: "database@123", // Mật khẩu
   database: "quanlybanhang", // Tên database
 })
 
 // Kết nối đến MySQL
 db.connect((err) => {
-    if (err) {
-      console.error("Error database:", err)
-    } else {
-      console.log("Connect database success")
-    }
+  if (err) {
+    console.error("Error database:", err)
+  } else {
+    console.log("Connect database success")
+  }
 })
 
 module.exports = db
--- src/controllers/SanPhamController.js
+++ src/controllers/SanPhamController.js
@@ -2,13 +2,13 @@ const db = require("../config/db");
 
 class SanPhamController {
     laySanPhamTheoLoai(req, res, next) {
-        const {idloaisanpham} = req.body;
+        const { idloaisanpham } = req.body;
         const query = "SELECT * FROM sanpham WHERE idloaisanpham=?";
         const values = [
             idloaisanpham
         ]
         db.query(query, values, (error, result, field) => {
-            if(error) {
+            if (error) {
                 return res.status(400).json({
                     error: error
                 })
@@ -22,13 +22,13 @@ class SanPhamController {
     }
 
     laySanPhamTheoId(req, res, next) {
-        const {id} = req.body;
+        const { id } = req.body;
         const query = "SELECT * FROM sanpham WHERE id=?";
         const values = [
             id
         ]
         db.query(query, values, (error, result, field) => {
-            if(error) {
+            if (error) {
                 return res.status(400).json({
                     error: error
                 })
@@ -43,8 +43,8 @@ class SanPhamController {
 
     layDsSanPham(req, res, next) {
         const query = "SELECT * FROM sanpham"
-        db.query(query, (error, result, field)=> {
-            if(error) {
+        db.query(query, (error, result, field) => {
+            if (error) {
                 return res.status(400).json({
                     error: error
                 })
@@ -58,50 +58,102 @@ class SanPhamController {
     }
 
     them(req, res, next) {
-        const {idloaisanpham, ten, dongia, mota} = req.body
-        const query = "INSERT INTO sanpham (idloaisanpham, ten, dongia, mota) VALUES (?, ?, ?, ?)";
-        const values = [idloaisanpham, ten, dongia, mota]
+        const { idloaisanpham, ten, dongia, mota } = req.body
+        const checkTenSanPham = /[~`!@#$%^&*()_\-+={}\[\]|\\:;"'<>,.?/]/.test(ten);
+        const checkDonGia = /^[0-9]+$/.test(dongia);
 
-        db.query(query, values, (error, result, field) => {
-            if(error) {
-                return res.status(400).json({
-                    error: error
-                })
-            } else {
+        if (checkTenSanPham) {
+            return res.status(200).json({
+                inputInvalid: "ten",
+                messageInvalid: "Tên sản phẩm không hợp lệ"
+            })
+        }
+        else if (!checkDonGia) {
+            return res.status(200).json({
+                inputInvalid: "dongia",
+                messageInvalid: "Đơn giá không hợp lệ"
+            })
+        }
+
+
+        const queryCheckTenSanPham = "SELECT * FROM sanpham WHERE ten = ?";
+        db.query(queryCheckTenSanPham, [ten], (error, results) => {
+            if (results.length > 0) {
                 return res.status(200).json({
-                    message: "Thêm sản phẩm thành công",
-                    sanPham: result.insertId
-                })
+                    inputInvalid: "ten",
+                    messageInvalid: "Tên sản phẩm đã tồn tại"
+                });
             }
-        })
+            const query = "INSERT INTO sanpham (idloaisanpham, ten, dongia, mota) VALUES (?, ?, ?, ?)";
+            const values = [idloaisanpham, ten, dongia, mota]
+
+            db.query(query, values, (error, result, field) => {
+                if (error) {
+                    return res.status(400).json({
+                        error: error
+                    })
+                } else {
+                    return res.status(200).json({
+                        message: "Thêm sản phẩm thành công",
+                        sanPham: result.insertId
+                    })
+                }
+            });
+        });
     }
 
     sua(req, res, next) {
-        const {idloaisanpham, ten, dongia, mota, id} = req.body
-        const query = "UPDATE sanpham SET idloaisanpham=?, ten=?, dongia=?, mota=? where id=?";
-        const values = [idloaisanpham, ten, dongia, mota, id]
+        const { idloaisanpham, ten, dongia, mota, id } = req.body
+        const checkTenSanPham = /[~`!@#$%^&*()_\-+={}\[\]|\\:;"'<>,.?/]/.test(ten);
+        const checkDonGia = /^[0-9]+$/.test(dongia);
 
-        db.query(query, values, (error, result, field) => {
-            if(error) {
-                return res.status(400).json({
-                    error: error
-                })
-            } else {
+        if (checkTenSanPham) {
+            return res.status(200).json({
+                inputInvalid: "ten",
+                messageInvalid: "Tên sản phẩm không hợp lệ"
+            })
+        }
+        else if (!checkDonGia) {
+            return res.status(200).json({
+                inputInvalid: "dongia",
+                messageInvalid: "Đơn giá không hợp lệ"
+            })
+        }
+
+
+        const queryCheckTenSanPham = "SELECT * FROM sanpham WHERE ten = ? and id != ?";
+        db.query(queryCheckTenSanPham, [ten, id], (error, results) => {
+            if (results.length > 0) {
                 return res.status(200).json({
-                    message: "Sửa sản phẩm thành công",
-                    sanPham: true
-                })
+                    inputInvalid: "ten",
+                    messageInvalid: "Tên sản phẩm đã tồn tại"
+                });
             }
-        })
+            const query = "UPDATE sanpham SET idloaisanpham=?, ten=?, dongia=?, mota=? where id=?";
+            const values = [idloaisanpham, ten, dongia, mota, id]
+
+            db.query(query, values, (error, result, field) => {
+                if (error) {
+                    return res.status(400).json({
+                        error: error
+                    })
+                } else {
+                    return res.status(200).json({
+                        message: "Sửa sản phẩm thành công",
+                        sanPham: true
+                    })
+                }
+            });
+        });
     }
 
     xoa(req, res, next) {
-        const {id} = req.body
+        const { id } = req.body
         const query = "DELETE FROM sanpham where id=?";
         const values = [id]
 
         db.query(query, values, (error, result, field) => {
-            if(error) {
+            if (error) {
                 return res.status(400).json({
                     error: error
                 })
